f2bfa57a9c9b6c4a8d0581115fde48b29b13a8d5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-30 11:31:15 +0000

    Updated Board.tsx

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import '../styles/Board.css';
+import Square from './Square';
 
-const Board = () => {
+const Board: React.FC = () => {
+    
   return (
-    <div className='Board'>Board</div>
+    <div className='Board'>
+        <div className='board-row'>
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div className='board-row'>
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div className='board-row'>
+            <Square />
+            <Square />
+            <Square />
+        </div>
+    </div>
   )
 }
 
