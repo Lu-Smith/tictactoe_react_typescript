26236578ad6a6770970d62ab2a777facf3815d45
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-30 10:29:29 +0000

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/Main.css';
+import Board from './Board';
 
 const Main = () => {
   return (
-    <div className='Main'>Main</div>
+    <div className='Main'>
+      <button>Play</button>
+      <div className="player">Player 1</div>
+      <Board />
+      <div className='results'>Results</div>
+    </div>
   )
 }
 
